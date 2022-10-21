0c169d845a22935bc7b84f91e6286947218b5094
ahmedlarbi <hamooda2002011@hotmail.com>
2022-10-22 03:37:04 +0400

    Added Sidebar & part of the Graph bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import GraphBar from './components/graphbar/Graphbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='title-div'>
+      <h1 className='title'> Internships </h1>
+    </div>
+      <Sidebar/>
+      <GraphBar/>
     </div>
   );
 }
---
+++ src/components/graphbar/Graphbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./styles.css"
+
+export default function GraphBar () {
+    return (
+        <div className='graphbar'>
+            <h2> Internship Insights</h2>
+        </div>
+    )
+}
