9cbd46d62e3d2420255abd3fce81323a4ebb23e7
ahmedlarbi <hamooda2002011@hotmail.com>
2022-10-22 06:29:03 +0400

    Title, Button and Paragraph to the Graph bar

--- src/components/graphbar/Graphbar.js
+++ src/components/graphbar/Graphbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./styles.css"
+import calendar from "../../assets/images/calendar-2.png"
 
 export default function GraphBar () {
     return (
@@ -9,6 +10,13 @@ export default function GraphBar () {
                 <p>   
                 In the eighteenth century the German philosopher Immanuel Kant developed a theory of knowledge in which knowledge about space can be both a priori and synthetic.
                 </p>
+            </div>
+            <div className='Filterbar'>
+                <div className='topbar'>
+                    <button className='weekButton'>This Week</button>
+                    <button className='monthButton'>This Month</button>
+                    <button className='datesButton'><img src={calendar} alt=""/>Select Dates</button>
+                </div>
             </div>
         </div>
     )
