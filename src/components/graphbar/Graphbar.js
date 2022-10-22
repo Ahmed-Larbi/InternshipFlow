52f348f1888c6f376e4f3883b1828cb10de64458
ahmedlarbi <hamooda2002011@hotmail.com>
2022-10-22 04:00:56 +0400

    Finished Sidebar

--- src/components/graphbar/Graphbar.js
+++ src/components/graphbar/Graphbar.js
@@ -4,7 +4,12 @@ import "./styles.css"
 export default function GraphBar () {
     return (
         <div className='graphbar'>
-            <h2> Internship Insights</h2>
+            <div className='infobar'>
+                <h2> Internship Insights</h2>
+                <p>   
+                In the eighteenth century the German philosopher Immanuel Kant developed a theory of knowledge in which knowledge about space can be both a priori and synthetic.
+                </p>
+            </div>
         </div>
     )
 }
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -6,35 +6,34 @@ import element3 from '../../assets/images/medal-star.png';
 import element2 from '../../assets/images/book.png';
 import element1 from '../../assets/images/briefcase.png';
 import element0 from '../../assets/images/setting-2.png';
+import hugPhoto from "../../assets/images/Rectangle18895.png"
 
 export default function Sidebar() {
     return (
                 <div>
                     <ul className='sidebar'>
                 <li>
-                        <span class="icon"><i class="fas fa-home"></i></span>
                         <img src={Image} alt="" className='radical-Logo'/>
                 </li>    
                 <li>
                     
-                        <span class="icon"><i class="fas fa-home"></i></span>
                         <span className="item"><img src={element4} alt="" className="elements"/>Dashboard</span>
                 </li>
                 <li>
-                        <span class="icon"><i class="fas fa-desktop"></i></span>
                         <span className="item"><img src={element3} alt="" className="elements"/>Apprenticeships</span>
                 </li>
                 <li>
-                        <span class="icon"><i class="fas fa-user-friends"></i></span>
                         <span className="item"><img src={element2} alt="" className="elements"/>Internships</span>
                 </li>
                 <li>
-                        <span class="icon"><i class="fas fa-tachometer-alt"></i></span>
+
                         <span className="item"><img src={element1} alt="" className="elements"/>Jobs</span>
                 </li>
                 <li>
-                        <span class="icon"><i class="fas fa-tachometer-alt"></i></span>
                         <span className="item"><img src={element0} alt="" className="elements"/>Settings</span>
+                </li>
+                <li>
+                        <span className="item profile "><img src={hugPhoto} alt="" className="elements profilePhoto"/>Adam Scott</span>
                 </li>
             </ul>
 
